686ed7ba70bf3cb7fbb1ceb3a44799c8c39ca23d
mikemonari <mikemonari31@gmail.com>
2022-07-05 09:41:15 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 
 /**
  * main - check the code.
- * Description: print_alphabet_x10 prints 10 times the alphabet, in lowercase
+ * print_alphabet_x10: 'prints 10 times the alphabet, in lowercase'
  * Return: Always 0.
  */
 
