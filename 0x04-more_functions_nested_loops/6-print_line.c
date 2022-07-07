12c9cc83ac0096670509edbf8fa630e4a0ed282e
mikemonari <mikemonari31@gmail.com>
2022-07-07 05:25:00 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,7 +9,10 @@ void print_line(int n)
 {
 	if (n <= 0)
 		_putchar('\n');
-	for (x = 0 ; x <= n ; x++)
-		_putchar('_');
+	else
+	{
+		for (x = 0 ; x < n ; x++)
+		_putchar(95);
 	_putchar('\n');
+	}
 }
