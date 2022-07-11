360688b9091605c22d32c029816c80ebf5f81874
mikemonari <mikemonari31@gmail.com>
2022-07-11 13:00:19 -0700

    getting characters of string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts2 - printscharacters of a string
+ * @str: parameter
+ */
+
+void puts2(char *str)
+{
+	int len;
+
+	len = strlen(str);
+	for (i = 0 ; i < len ; i+=2)
+		_putchar(str[1]);
+	_putchar('\n');
+}
