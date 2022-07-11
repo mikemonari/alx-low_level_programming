104dc11594cd0b6e0585308207e7cfc0e20804dc
mikemonari <mikemonari31@gmail.com>
2022-07-11 13:32:14 -0700

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <string.h>
 /**
  * puts_half - prints half of a string
  * @str: parameter
@@ -7,7 +7,7 @@
 
 void puts_half(char *str)
 {
-	int x, y;
+	int x, y, i;
 
 	x = strlen(str);
 	if (x % 2 == 1)
