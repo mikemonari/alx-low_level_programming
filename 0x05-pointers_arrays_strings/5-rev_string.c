374ce8a9ad581c634e661ae6621042a0bb48d112
mikemonari <mikemonari31@gmail.com>
2022-07-11 12:43:35 -0700

    reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,7 +13,7 @@ void rev_string(char *s)
 	length1 = 0;
 	length2 = 0;
 
-	while (s[length1]] != '\0')
+	while (s[length1] != '\0')
 		length++;
 
 	length2 = length1 -1
@@ -25,4 +25,3 @@ void rev_string(char *s)
 		length2 -= 1;
 	}
 }
-
