edaf088a661e09655e0a5dc429b5c6f712c6b5bb
mikemonari <mikemonari31@gmail.com>
2022-07-13 00:49:12 -0700

    comparing strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strcmp - function that compares a string
+ * @s1: first parameter
+ * @s2: second parameter
+ * Return: interger
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+}
