67ffdc8be52e08f82e122b556aa74efdd3c6c60f
mikemonari <mikemonari31@gmail.com>
2022-07-22 05:48:06 -0700

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments passed into it
+ * @argc: number of command line arguments
+ * @argv: an array containing the program command line arguments
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+C - args and agrv
